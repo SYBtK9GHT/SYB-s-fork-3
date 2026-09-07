3d04618e674b43fa8338c0ce5d002fef672e2a27
SYBtK9GHT <sybren.jonkheid@gmail.com>
2026-09-07 10:40:59 +0200

    Update sketch.js
    naam en schaakboord gemaakt

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,7 +1,21 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1250, 600);
 }
 
 function draw() {
   background(220);
+
+  fill(5,65,255);
+  textSize(17);
+  text(":3\nHello World!\nthis project belong to Sybren Jonkheid", 5, 20);
+
+  fill(255);
+  square(20,100,100);
+
+  fill(0);
+  square(20,100,33);
+  square(20+66,100,33);
+  square(20,100+66,33);
+  square(20+33,100+33,33);
+  square(20+66,100+66,33);
 }
