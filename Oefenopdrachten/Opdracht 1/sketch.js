1defa82b2ba3f95192e3f0983ebbefde187e9ce1
SYBtK9GHT <sybren.jonkheid@gmail.com>
2026-09-07 11:02:11 +0200

    Update sketch.js
    vlag toegevoegd en geleerd te nesten

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -5,17 +5,45 @@ function setup() {
 function draw() {
   background(220);
 
+  //naam
   fill(5,65,255);
   textSize(17);
   text(":3\nHello World!\nthis project belong to Sybren Jonkheid", 5, 20);
 
+  //vlag
+  push();
+  translate(20,100);
+
+  noStroke();
+
+  fill(255,0,0);
+  rect(0,0,150,33);
+
   fill(255);
-  square(20,100,100);
+  rect(0,33,150,33);
+
+  fill(0,0,255);
+  rect(0,66,150,33);
+
+  pop();
+  
+  //schaakboord
+  push();
+  translate(20,250);
+
+  stroke(0);
+
+  fill(255);
+  square(0,0,100);
 
   fill(0);
-  square(20,100,33);
-  square(20+66,100,33);
-  square(20,100+66,33);
-  square(20+33,100+33,33);
-  square(20+66,100+66,33);
+  square(0,0,33);
+  square(66,0,33);
+  square(0,66,33);
+  square(33,33,33);
+  square(66,66,33);
+
+  pop();
+
+
 }
