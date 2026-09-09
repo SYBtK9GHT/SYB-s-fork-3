e96814f8b66794d65f104d4720f182c48c7c7c1f
SYBtK9GHT <sybren.jonkheid@gmail.com>
2026-09-09 09:12:57 +0200

    aan week 3 opdracht beggonen

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,7 +1,24 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(175);
 }
 
+const px = 180
+
 function draw() {
-  background(220);
+
+  grid = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+  ]
+  for (let y = 0; y < grid.length; y++){
+    for (let x = 0; x < grid[y].length; x++){
+      fill(75,75,75);
+      const pos_x = x*px+20*x+10;
+      const pos_y = y*px+20*y+10;
+      square(pos_x, pos_y, px, 10);
+      
+    }
+  }
 }
