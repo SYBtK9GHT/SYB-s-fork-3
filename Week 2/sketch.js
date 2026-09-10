0cee34fd9bc3dcd0fdb06ade261cb53af3a2a59a
SYBtK9GHT <sybren.jonkheid@gmail.com>
2026-09-10 14:31:59 +0200

    aan week 2 beggonen

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,3 +1,9 @@
+  const lights = [
+    color(0,255,0),
+    color(225,0,0),
+    color(255,125,0)
+  ];
+
 function setup() {
   createCanvas(1000, 600);
 }
@@ -29,12 +35,30 @@ function draw() {
   push();
   translate(0,480);
 
+  
+
+  fill(100);
+  triangle(100,0,400,0,250,-350);
+  triangle(400,0,800,0,600,-300);
+
   fill(125);
-
   triangle(0,0,150,0,75,-150);
+  triangle(300,0,500,0,400,-250);
+  triangle(725,0,875,0,800,-150);
 
+  pop();
 
+  push();
+  translate(10,10);
+
+  let active_lights = [
+    color(0),
+    color(0),
+    color(0)
+  ];
+  fill(200);
+  rect(0,0,50,150);
 
   
-  pop();
+
 }
--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,10 +1,11 @@
 const px = 180;
 let win = false;
 let turn = false
+let start = 0;
 let grid = [
-  [0,0,0],
-  [0,0,0],
-  [0,0,0]
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]
 ];
 
 
@@ -19,56 +20,74 @@ function draw() {
 
   const colors = [
     color(75),
-    color(255,0,0),
-    color(0,0,255)
+    color(255, 0, 0),
+    color(0, 0, 255)
   ];
 
-  for (let y = 0; y < grid.length; y++){
-    for (let x = 0; x < grid[y].length; x++){
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
       fill(colors[grid[y][x]]);
-      const pos_x = x*px+20*x+10;
-      const pos_y = y*px+20*y+10;
+      const pos_x = x * px + 20 * x + 10;
+      const pos_y = y * px + 20 * y + 10;
       square(pos_x, pos_y, px, 10);
     }
   }
-}
-
-function mouseClicked(){
-
-  for (let y = 0; y < grid.length; y++){
-    for (let x = 0; x < grid[y].length; x++){
-      const pos_x = x*px+20*x+10;
-      const pos_y = y*px+20*y+10;      
-
-      if (mouseX > pos_x && mouseY > pos_y && mouseX < pos_x+px && mouseY < pos_y+px){
-        if (grid[y][x] == 0){
-          if (turn){
-            grid[y][x] = 2;
-          } else {
-            grid[y][x] = 1;
-          }
-          turn = !turn;
-        }
-
-        if ((grid[y][0]== grid[y][1] && grid[y][2] == grid[y][1] && grid[y][2]!=0)||(grid[0][x]== grid[1][x] && grid[2][x] == grid[1][x] && grid[2][x]!=0)||(grid[0][0]==grid[1][1]&&grid[1][1]==grid[2][2]&&grid[0][0]!=0)||(grid[0][2]==grid[1][1]&&grid[1][1]==grid[2][0]&&grid[0][2]!=0)){
-          win = true;
-          turn = !turn;
-        }
+  let restart = true;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] == 0) {
+        restart = false;
       }
     }
   }
-  
-      if (win){
-        for (let i = 0; i < grid.length; i++){
-          for(let j = 0; j < grid[i].length; j++){
-          if (turn){
-            console.log(turn)
+  if (restart) {
+    setTimeout(() => {
+      grid = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+      win = false;
+      restart = false;
+    }, 5000);
+
+  }
+}
+
+
+
+
+function mouseClicked() {
+  if (!win) {
+    for (let y = 0; y < grid.length; y++) {
+      for (let x = 0; x < grid[y].length; x++) {
+        const pos_x = x * px + 20 * x + 10;
+        const pos_y = y * px + 20 * y + 10;
+
+        if (mouseX > pos_x && mouseY > pos_y && mouseX < pos_x + px && mouseY < pos_y + px) {
+          if (grid[y][x] == 0) {
+            if (turn) {
+              grid[y][x] = 2;
+            } else {
+              grid[y][x] = 1;
+            }
+            turn = !turn;
+          }
+
+          if ((grid[y][0] == grid[y][1] && grid[y][2] == grid[y][1] && grid[y][2] != 0) || (grid[0][x] == grid[1][x] && grid[2][x] == grid[1][x] && grid[2][x] != 0) || (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] && grid[0][0] != 0) || (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0] && grid[0][2] != 0)) {
+            win = true;
+            turn = !turn;
+          }
+        }
+      }
+    }
+
+    if (win) {
+      for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+          if (turn) {
             grid[i][j] = 2;
           } else {
-            console.log(turn)
             grid[i][j] = 1;
           }
         }
       }
     }
+  }
 }
