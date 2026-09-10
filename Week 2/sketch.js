8663803a2ef6a5d8dbf889301eac85e15653e868
SYBtK9GHT <sybren.jonkheid@gmail.com>
2026-09-10 11:47:58 +0200

    tic tac toe gemaakt en week 2 geskipt

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,7 +1,40 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 600);
 }
 
 function draw() {
-  background(220);
+  background(175,225,255);
+  noStroke();
+
+  push();
+  translate(0,500);
+
+  fill(75);
+  rect(0,0,1000,100);
+
+  fill(25, 125, 50);
+  rect(0,-10,1000,10);
+
+  fill(25, 255, 50);
+  rect(0,-20,1000,10);
+
+  fill(255);
+
+  for (i = 0; i < 11; i++){
+    rect(-25+100*i, 45, 50, 10, 5);
+  }
+  pop();
+  
+
+  push();
+  translate(0,480);
+
+  fill(125);
+
+  triangle(0,0,150,0,75,-150);
+
+
+
+  
+  pop();
 }
--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,24 +1,76 @@
+const px = 180;
+let win = false;
+let turn = false
+let grid = [
+  [0,0,0],
+  [0,0,0],
+  [0,0,0]
+];
+
+
 function setup() {
   createCanvas(600, 600);
   background(175);
 }
 
-const px = 180
+
 
 function draw() {
 
-  grid = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
-  ]
+  strokeWeight(5);
+
+  const colors = [
+    color(75),
+    color(255,0,0),
+    color(0,0,255)
+  ];
+
   for (let y = 0; y < grid.length; y++){
     for (let x = 0; x < grid[y].length; x++){
-      fill(75,75,75);
+      fill(colors[grid[y][x]]);
       const pos_x = x*px+20*x+10;
       const pos_y = y*px+20*y+10;
       square(pos_x, pos_y, px, 10);
-      
     }
   }
+}
+
+function mouseClicked(){
+
+  for (let y = 0; y < grid.length; y++){
+    for (let x = 0; x < grid[y].length; x++){
+      const pos_x = x*px+20*x+10;
+      const pos_y = y*px+20*y+10;      
+
+      if (mouseX > pos_x && mouseY > pos_y && mouseX < pos_x+px && mouseY < pos_y+px){
+        if (grid[y][x] == 0){
+          if (turn){
+            grid[y][x] = 2;
+          } else {
+            grid[y][x] = 1;
+          }
+          turn = !turn;
+        }
+
+        if (grid[y][0]== grid[y][1] && grid[y][2] == grid[y][1] && grid[y][2]!=0){
+          win = true;
+          turn = !turn;
+        }
+      }
+    }
+  }
+  
+      if (win){
+        for (let i = 0; i < grid.length; i++){
+          for(let j = 0; j < grid[i].length; j++){
+          if (turn){
+            console.log(turn)
+            grid[i][j] = 2;
+          } else {
+            console.log(turn)
+            grid[i][j] = 1;
+          }
+        }
+      }
+    }
 }
