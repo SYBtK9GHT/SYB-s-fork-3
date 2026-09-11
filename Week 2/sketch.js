6cd49a920dd7f4590584da39add53f55c3dddd37
SYBtK9GHT <sybren.jonkheid@gmail.com>
2026-09-11 12:25:54 +0200

    verder aan week 2 gegaan

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,8 +1,4 @@
-  const lights = [
-    color(0,255,0),
-    color(225,0,0),
-    color(255,125,0)
-  ];
+let light_switch = 0;
 
 function setup() {
   createCanvas(1000, 600);
@@ -49,16 +45,47 @@ function draw() {
   pop();
 
   push();
-  translate(10,10);
+  translate(925,300);
 
+   const lights = [
+     color(0,255,0),
+     color(255,125,0),
+     color(225,0,0)
+   ];
   let active_lights = [
     color(0),
     color(0),
     color(0)
   ];
-  fill(200);
+  fill(100);
   rect(0,0,50,150);
+  rect(12,0,26,180)
 
-  
+  for (let i = 0; i < active_lights.length; i++){
+    active_lights[i] = color(0,0,0,0);
+  }
+
+  active_lights[light_switch] = lights[light_switch];
+
+  fill(active_lights[0]);
+  circle(25,125,40);
+  fill(active_lights[1]);
+  circle(25,75,40);
+  fill(active_lights[2]);
+  circle(25,25,40);
+
+  pop();
+
+
 
 }
+
+  function keyPressed (){
+    if (key === 13 || key === 32){
+      light_switch++;
+    }
+    if (light_switch >= 3){
+      light_switch = 0;
+    }
+    console.log(light_switch);
+  }
