b1288b6266fcd63cd804a2d685640c7deaa702fd
SYBtK9GHT <sybren.jonkheid@gmail.com>
2026-09-15 09:53:34 +0200

    autos gedebugged && dev tools zijn nog zichtbaar

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -15,45 +15,40 @@ class car {
 
 let cars = []
 let prepos = 0
-for (let i = 0; i < 25; i++){
-  prepos = Math.floor(Math.random()*150)+prepos+30
-  cars.push(new car(!!Math.floor(Math.random()*1.9), 10, -135*i-prepos, Math.floor(Math.random()*5)))
+for (let i = 0; i < 10; i++) {
+  prepos = Math.floor(Math.random() * 150) + prepos + 30
+  cars.push(new car(!!Math.floor(Math.random() * 1.9), 10, -135 * i - prepos, Math.floor(Math.random() * 5)))
 }
 
-function draw_car(colors,pos,y){
-    const car_colors = [
-    color(255,0,0),
-    color(255,255,0),
-    color(0,255,0),
-    color(125,0,255),
-    color(0,0,255)
+function draw_car(colors, pos) {
+  const car_colors = [
+    color(255, 0, 0),
+    color(255, 255, 0),
+    color(0, 255, 0),
+    color(125, 0, 255),
+    color(0, 0, 255)
   ];
-  
-    fill(car_colors[colors]);
-    rect(pos-25, y-10, 125,40);
-    rect(pos, y-40, 60,40);
-    fill(50);
-    circle(pos, y+30, 30)
-    circle(pos+75, y+30, 30);
 
-    
+  fill(car_colors[colors]);
+  rect(pos -125, -10, 125, 40);
+  rect(pos -100, -40, 60, 40);
+  fill(50);
+  circle(pos -100, 30, 30)
+  circle(pos -25, 30, 30);
+
+  fill(255,0,255)
+  rect(pos,-70,2,100)
+
 }
 
 
 function draw() {
 
-
-
   background(175, 225, 255);
   noStroke();
 
-
-
-
   push();
   translate(0, 480);
-
-
 
   fill(100);
   triangle(100, 0, 400, 0, 250, -350);
@@ -118,48 +113,77 @@ function draw() {
 
 
 
+
   for (let i = 0; i < cars.length; i++) {
     let base_speed = 7.5;
     let my_car = cars[i];
     let acc = 0.05
+    const after_stop = 935;
+    const stop = 750
 
-    if (cars.pos > 500){
-      my_car.spd = lerp(my_car.spd, base_speed*12, 0.95);
+    fill(255,0,255);
+    rect(stop,0,2,100);
+    rect(after_stop,0,2,100);
+
+    //change spped with light
+    if (cars[i].pos > after_stop) {
+      my_car.spd = lerp(my_car.spd, base_speed, 0.2);
     } else {
-    switch(light_switch){
-      case 0: {my_car.spd = lerp(my_car.spd, base_speed, acc)} break;
-      case 1: {my_car.spd = lerp(my_car.spd, (base_speed/2), acc)}   break;
-      case 2: {my_car.spd = lerp(my_car.spd, 0, acc)} break;  
-    }}
-    
-    let spd = cars[i].spd;
-    let y = 50;
-    if (my_car.lane) {
-      y -= 50
-      spd *= 1.5
-    }
-    my_car.pos += spd
-    draw_car(my_car.clr,my_car.pos,y)
-    
-    textSize(25)
-    fill(255,0,255)
-    text(i,my_car.pos,0);
-    
-
-    for (let j = 0; j > cars.length; j++){
-      if (cars[j].pos < prepos){
-        prepos = my_car.pos
+      switch (light_switch) {
+        case 0: { my_car.spd = lerp(my_car.spd, base_speed, acc) } break;
+        case 1: { my_car.spd = lerp(my_car.spd, (base_speed / 2), acc) } break;
+        case 2: { if (cars[i].pos > stop) { my_car.spd = lerp(my_car.spd, 0, acc * 1.5) } } break;
       }
     }
 
-    if (my_car.pos > 1100){
-      my_car.pos = -200-prepos;
-      my_car.color = Math.floor(Math.random()*5)
-      my_car.lane = !!Math.floor(Math.random()*1.9)
+    //left lane boost
+    let spd = cars[i].spd;
+    if (my_car.lane) {
+      spd *= 1.5
+    }
+
+    // brakes
+  
+    for (let j = 0; j < cars.length; j++) {
+      if (my_car != cars[j] && my_car.lane == cars[j].lane && my_car.pos > cars[j].pos - 185 && my_car.pos < cars[j].pos) {
+        my_car.spd = lerp(my_car.spd, 0, acc * 3.5);
+      }
+    }
+
+    my_car.pos += spd;
+
+
+    //respawn
+    if (my_car.pos > 1100) {
+      my_car.pos = -550 * 1.2;
+      my_car.clr = Math.floor(Math.random() * 15 / 3)
+      my_car.lane = !!Math.floor(Math.random() * 1.9)
     }
   }
 
+  //render right lane infront off left lane
+  let cars_l = [];
+  let cars_r = [];
+  for (let i = 0; i < cars.length; i++) {
+    if (cars[i].lane) {
+      cars_l.push(cars[i])
+    } else {
+      cars_r.push(cars[i])
+    }
+  }
+  for (let i = 0; i < cars_l.length; i++) {
+    my_car = cars_l[i]
+    draw_car(my_car.clr, my_car.pos);
+  }
+  push();
+  translate(0, 50);
+  for (let i = 0; i < cars_r.length; i++) {
+    my_car = cars_r[i]
+    draw_car(my_car.clr, my_car.pos);
+  }
+
   pop();
+
 }
 
 function keyPressed() {
